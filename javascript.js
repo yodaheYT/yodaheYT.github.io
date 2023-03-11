f0a1757a1a67f478b3fdc0185d625a22aae92764
yodaheYT <94496398+yodaheYT@users.noreply.github.com>
2023-03-10 17:20:33 -0700

    color + 404 changes

--- javascript.js
+++ javascript.js
@@ -13,13 +13,13 @@ setTimeout(() => {
     const GITHUBELEM = document.getElementById("GitHub");
     const YOUTUBEELEM = document.getElementById("YouTube");
     const TWITCHELEM = document.getElementById("Twitch");
-    const COUNTDOWNELEM = document.getElementById("Countdown");
+    // const COUNTDOWNELEM = document.getElementById("Countdown");
     //MAIN CODE
     DISCORDELEM.addEventListener('click', () => {nav(DISCORDURL)});
     GITHUBELEM.addEventListener('click', () => {nav(GITHUBURL)});
     YOUTUBEELEM.addEventListener('click', () => {nav(YOUTUBEURL)});
     TWITCHELEM.addEventListener('click', () => {nav(TWITCHURL)});
-    COUNTDOWNELEM.addEventListener('click', () => {window.location.href = `/countdown`})
+    // COUNTDOWNELEM.addEventListener('click', () => {window.location.href = `/countdown`})
 
     //ELEMSB
     const observer = new IntersectionObserver((entries) => {
@@ -38,30 +38,30 @@ setTimeout(() => {
     hidden.forEach((el) => observer.observe(el));
 
     //Countdown
-    const countDownElem = document.getElementById("countdowntxt");
-    const countDownDate = new Date(2023, 0, 1, 00, 00);
-    const endTime = countDownDate.getTime();
-    let f = setInterval(() => {
-        let todayDate = new Date();
-        let todayTime = todayDate.getTime();
-        let remainingTime = endTime - todayTime;
-        let oneMin = 60 * 1000;
-        let oneHr = 60 * oneMin;
-        let oneDay = 24 * oneHr;
+    // const countDownElem = document.getElementById("countdowntxt");
+    // const countDownDate = new Date(2023, 0, 1, 00, 00);
+    // const endTime = countDownDate.getTime();
+    // let f = setInterval(() => {
+    //     let todayDate = new Date();
+    //     let todayTime = todayDate.getTime();
+    //     let remainingTime = endTime - todayTime;
+    //     let oneMin = 60 * 1000;
+    //     let oneHr = 60 * oneMin;
+    //     let oneDay = 24 * oneHr;
 
-        let addZeroes = (num) => (num < 10 ? `0${num}` : num);
-        if (endTime < todayTime) {
-            clearInterval(f);
-            countDownElem.innerText = "🎉Happy 2023";
-        } else {
-            let daysLeft = Math.floor(remainingTime / oneDay);
-            let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
-            let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
-            let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+    //     let addZeroes = (num) => (num < 10 ? `0${num}` : num);
+    //     if (endTime < todayTime) {
+    //         clearInterval(f);
+    //         countDownElem.innerText = "🎉Happy 2023";
+    //     } else {
+    //         let daysLeft = Math.floor(remainingTime / oneDay);
+    //         let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+    //         let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+    //         let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
 
-            countDownElem.innerText = `${daysLeft} Days, ${hrsLeft} Hours, ${minsLeft} Mins, and ${secsLeft} Seconds`
-        }
-    }, 1000);
-    console.log(countDownDate);
-    console.log(endTime);
+    //         countDownElem.innerText = `${daysLeft} Days, ${hrsLeft} Hours, ${minsLeft} Mins, and ${secsLeft} Seconds`
+    //     }
+    // }, 1000);
+    // console.log(countDownDate);
+    // console.log(endTime);
 }, 500);
